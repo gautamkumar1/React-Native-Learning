993a144c8d8526fc4d83a7d63b93540ff38c07a9
gautamkumar1 <gautamraaz936@gmail.com>
2025-05-04 20:18:22 +0530

    Created login user api and fixed some issue

--- server/src/controllers/user-controller.js
+++ server/src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../modules/user-model.js";
+import jwt from "jsonwebtoken";
 export const registerUser = async (req, res) => {
     try {
         const {email, password} = req.body;
@@ -18,7 +19,30 @@ export const registerUser = async (req, res) => {
         const isUserCreated = await User.findOne({email}).select('-password');
         return res.status(201).json({message: 'User created successfully', user: isUserCreated});
     } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: 'Internal server error'})
         
     }
 }
 
+export const loginUser = async (req, res) => {
+    try {
+        const {email,password} = req.body;
+        const isUserExist = await User.findOne({email});
+        if (!isUserExist) {
+            return res.status(400).json({message: 'User does not exist'})
+        }
+        const isPasswordMatch = await bcrypt.compare(password, isUserExist.password);
+        if (!isPasswordMatch) {
+            return res.status(400).json({message: 'Invalid credentials'})
+        }
+        const token = jwt.sign({isUserExist}, process.env.JWT_SECRET,{
+            expiresIn: process.env.JWT_EXPIRES
+        });
+        return res.status(200).json({message: 'Login successful', token})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: 'Internal server error'})
+        
+    }
+}
--- server/src/routes/user-route.js
+++ server/src/routes/user-route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user-controller.js";
+import { loginUser, registerUser } from "../controllers/user-controller.js";
 const router = Router();
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 export default router
