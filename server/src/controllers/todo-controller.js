85f1da6d4fe0005281f1b184ded978b559c3ef39
gautamkumar1 <gautamraaz936@gmail.com>
2025-05-04 20:50:03 +0530

    Created delete and getalltodo controller

--- server/src/controllers/todo-controller.js
+++ server/src/controllers/todo-controller.js
@@ -12,3 +12,25 @@ export const addTodo = async (req, res) => {
         
     }
 }
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const {id} = req.params
+        const todo = await Todo.findByIdAndDelete(id)
+        return res.status(200).json({message: 'Todo deleted successfully', todo})
+
+    } catch (error) {
+        console.log(`Error while deleting todo ${error}`);
+        return res.status(500).json({message: 'Getting error while deleting todo'})
+    }
+}
+
+export const getAllTodo = async (req, res) => {
+    try {
+        const todos = await Todo.find({});
+        return res.status(200).json({message: 'Success', todos})
+    } catch (error) {
+        console.log(`Error while getting todos ${error}`);
+        return res.status(500).json({message: 'Getting error while getting todos'})
+    }
+}
--- server/src/routes/todo-routes.js
+++ server/src/routes/todo-routes.js
@@ -1,6 +1,8 @@
 import {Router} from "express"
-import { addTodo } from "../controllers/todo-controller.js";
+import { addTodo, deleteTodo, getAllTodo } from "../controllers/todo-controller.js";
 import { isAuthenticated } from "../middleware/auth-middleware.js";
 const router = Router();
 router.post('/add', isAuthenticated,addTodo)
+router.delete('/delete/:id', isAuthenticated,deleteTodo)
+router.get('/get', isAuthenticated,getAllTodo)
 export default router
