b6148d498c0447689a4f850e6b67fef34258a929
gautamkumar1 <gautamraaz936@gmail.com>
2025-05-14 22:59:40 +0530

    Add react native toast

--- todo/store/authStore.ts
+++ todo/store/authStore.ts
@@ -1,6 +1,7 @@
 import { User } from "@/types/User";
 import { create } from "zustand";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from "react-native-toast-message";
 interface AuthStore {
   user: User | null;
   isAuthenticated: () => boolean;
@@ -33,12 +34,22 @@ export const useAuthStore = create<AuthStore>((set, get) => ({
             body: JSON.stringify({ email, password }),
         });
         if(response.ok){
+          Toast.show({
+            type: "success",
+            text1: "Success!",
+            text2: "Registered successfully",
+          });
             const data = await response.json();
             set({ token: data.token });
             set({ user: data });
-        }
+          }
     } catch (error) {
         console.error(error, "Error registering user");
+        Toast.show({
+          type: "error",
+          text1: "Error!",
+          text2: "Error registering user",
+        });
     } finally {
       set({ isRegisterLoading: false });
     }
@@ -52,6 +63,11 @@ export const useAuthStore = create<AuthStore>((set, get) => ({
         body: JSON.stringify({ email, password }),
       });
       if(response.ok){
+        Toast.show({
+          type: "success",
+          text1: "Success!",
+          text2: "Logged in successfully",
+        });
         const data = await response.json();
         await AsyncStorage.setItem("token", data.token);
         set({ token: data.token });
@@ -59,6 +75,11 @@ export const useAuthStore = create<AuthStore>((set, get) => ({
       }
     } catch (error) {
       console.error(error, "Error logging in");
+      Toast.show({
+        type: "error",
+        text1: "Error!",
+        text2: "Error logging in",
+      });
     } finally {
       set({ isLoginLoading: false });
     }
